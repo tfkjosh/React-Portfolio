04689b52a8d8e955e9686ed4259b8fc94d1c6a0a
joshua-king <jkg050604@gmail.com>
2024-11-13 21:00:07 -0600

    Revert "profile"
    This reverts commit de9669dd0dc54f57308e96be2b60cea06dc2d0d6.

--- portfolio/src/components/Section.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import clsx from "clsx";
-export const Section = (props) => {
-    return (_jsx("div", { ...props, className: clsx("md:mx-[10%] border-b border-b-white/20 px-8 py-24", props.className), children: props.children }));
-};
-//# sourceMappingURL=Section.js.map
